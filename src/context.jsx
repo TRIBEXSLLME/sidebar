e3e605d2277431245641eaff3a76ce6e86a5fa6d
Khalid ibrahim <masterpiekhalid@gmail.com>
2024-08-28 17:05:30 +0100

    first commit

---
+++ src/context.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSideBar = () => {
+    console.log("hiiii")
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    console.log('hello');
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openSideBar,
+        openModal,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+//custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
